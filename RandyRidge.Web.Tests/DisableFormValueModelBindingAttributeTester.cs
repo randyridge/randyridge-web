94db5cffa6f01aab04bd40e46e13b045f51f20d0
Randy Ridge <randyridge@gmail.com>
2019-12-06 11:18:08 -0500

    added an attribute, added target for netstandard2.1

---
+++ RandyRidge.Web.Tests/DisableFormValueModelBindingAttributeTester.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using Shouldly;
+using Xunit;
+
+namespace RandyRidge.Web {
+    public static class DisableFormValueModelBindingAttributeTester {
+        private static readonly DisableFormValueModelBindingAttribute Attribute = new DisableFormValueModelBindingAttribute();
+
+        public static class OnResourceExecuted {
+            [Fact]
+            public static void coverage() {
+                Attribute.OnResourceExecuted(new ResourceExecutedContext(new ActionContext(new DefaultHttpContext(), new RouteData(), new ActionDescriptor()), new List<IFilterMetadata>()));
+            }
+        }
+
+        public static class OnResourceExecuting {
+            [Fact]
+            public static void removes_form_value_provider() {
+                VerifyRemoval(BuildContext(new FormValueProviderFactory()));
+            }
+
+            [Fact]
+            public static void removes_jquery_form_value_provider() {
+                VerifyRemoval(BuildContext(new JQueryFormValueProviderFactory()));
+            }
+
+            private static void VerifyRemoval(ResourceExecutingContext ctx) {
+                ctx.ValueProviderFactories.Count.ShouldBe(1);
+                Attribute.OnResourceExecuting(ctx);
+                ctx.ValueProviderFactories.Count.ShouldBe(0);
+            }
+        }
+
+        private static ResourceExecutingContext BuildContext(IValueProviderFactory providerFactory) =>
+            new ResourceExecutingContext(new ActionContext(new DefaultHttpContext(), new RouteData(), new ActionDescriptor()), new List<IFilterMetadata>(), new List<IValueProviderFactory> {
+                providerFactory
+            });
+    }
+}
