eef4288153b0566b28b176ff894f5eeb0e64fce7
Randy Ridge <randyridge@gmail.com>
2019-12-06 11:20:00 -0500

    added an attribute, added target for netstandard2.1 (#2)

---
+++ RandyRidge.Web/DisableFormValueModelBindingAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using RandyRidge.Common;
+
+namespace RandyRidge.Web {
+    /// <summary>
+    ///     Attribute to disable form value model binding.
+    /// </summary>
+    /// <remarks>
+    ///     Used to more efficiently stream file content instead of the
+    ///     default model binding that loads the body into memory.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter {
+        /// <inheritdoc />
+        public void OnResourceExecuted(ResourceExecutedContext context) {
+        }
+
+        /// <inheritdoc />
+        public void OnResourceExecuting(ResourceExecutingContext context) {
+            context = Guard.ArgumentNotNull(context, nameof(context));
+            RemoveFormValueProvider<FormValueProviderFactory>(context);
+            RemoveFormValueProvider<JQueryFormValueProviderFactory>(context);
+        }
+
+        private static void RemoveFormValueProvider<T>(ResourceExecutingContext context) where T : IValueProviderFactory {
+            var valueProviderFactory = context.ValueProviderFactories
+                .OfType<T>()
+                .FirstOrDefault();
+
+            if(valueProviderFactory != null) {
+                context.ValueProviderFactories.Remove(valueProviderFactory);
+            }
+        }
+    }
+}
